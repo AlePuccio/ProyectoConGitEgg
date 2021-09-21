03679d83cc3769a7c8292f705b0e3fa441119d0c
AlePuccio <ale.eze.puccio@gmail.com>
2021-09-21 20:33:57 -0300

    primer commit proyecto egg

---
+++ src/proyectocongit/ProyectoConGit.java
@@ -0,0 +1,17 @@
+
+package proyectocongit;
+
+/**
+ *
+ * @author gae
+ */
+public class ProyectoConGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
